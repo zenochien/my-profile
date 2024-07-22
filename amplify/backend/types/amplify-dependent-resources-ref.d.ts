f1f7b67600e8a61d6a1c2d590b80ef5d3d2884ca
zenochien <48956792+zenochien@users.noreply.github.com>
2024-07-22 18:12:24 +0700

    update

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "myprofile": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
